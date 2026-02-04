57de069ba3142d0972ea45380700161134795422
artur.guttman <artur.guttman@btrl.ro>
2026-02-04 13:09:24 +0200

    Add project files.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+﻿# ---- build ----
+FROM docker.io/library/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY . .
+RUN dotnet publish -c Release -o /app
+
+# ---- runtime ----
+FROM docker.io/library/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build /app ./
+EXPOSE 8080
+ENTRYPOINT ["dotnet", "dotnet-scale-poc.dll"]
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+int desiredReplicas = 1;
+
+// ✅ FOARTE IMPORTANT: args
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/set", (int value) =>
+{
+    if (value < 1)
+        value = 1;
+
+    desiredReplicas = value;
+    Console.WriteLine($"Desired replicas set to {desiredReplicas}");
+    return desiredReplicas;
+});
+
+app.MapGet("/desired-replicas", () =>
+{
+    return desiredReplicas;
+});
+
+app.MapGet("/", () => "App is running");
+
+
+// ✅ NU mai hardcoda URL aici
+app.Run();
